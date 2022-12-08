ab7e837d8701275a1935e76a0effd7587d256711
TylerAmey <89492388+TylerAmey@users.noreply.github.com>
2022-12-08 14:54:13 -0500

    Current Classes module

---
+++ CurrentClasses/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrentClasses
+{
+    public partial class CurrentClasses : Form
+    {
+        public CurrentClasses()
+        {
+            InitializeComponent();
+        }
+
+
+        /*Functionality:
+         * 
+         * Read the current classes within the Student class
+         * Add all classes to the classesFlowLayoutPanel
+         * Take all class times from each class and add it to the calendar, as well as the names
+         * A class time is shown as used when the panel turns green
+         * The Class name will also be on the panel
+         * 
+         */
+    }
+}
