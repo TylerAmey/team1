84aa71641721b31a1bb54473bc1f5c43364b9107
ndw1117 <ndw1117@g.rit.edu>
2022-12-08 17:48:26 -0500

    Suggestion logic in progress (Nick)

--- SuggestedClasses/Form1.cs
+++ SuggestedClasses/Form1.cs
@@ -16,7 +16,7 @@ namespace SuggestedClasses
     //This form will take a list of all courses' sections and go through them, choosing 5 to display based on characteristics of the course/section.
     public partial class Form1 : Form 
     {
-
+        List<Section> recommendedSections;
         public Form1()
         {
             InitializeComponent();
@@ -65,7 +65,29 @@ namespace SuggestedClasses
 
         public Form1(List<Course> courses) //receives the list of all courses
         {
+            Section temp; //temp section variable for sorting the list of recommended sections 
+            int i = 0; //iterator variable
+            int j = 0; //iterator varibale
 
+            //initialize the recommended list to contain the first sections of the first 3 existing courses
+            for(i = 0; i < 3; i++)
+            {
+                recommendedSections[i] = courses[i].sections[0];
+            }
+
+            //sorts the initial recommended list in descending order by their recValue
+            for(i = 0; i < 3; i++)
+            {
+                for (j = i + 1; j < 3; j++)
+                {
+                    if (CalcRecValue(recommendedSections[i]) < CalcRecValue(recommendedSections[j]))
+                    {
+                        temp = recommendedSections[i];
+                        recommendedSections[i] = recommendedSections[j];
+                        recommendedSections[j] = temp;
+                    }
+                }
+            }
 
         }
 
@@ -74,7 +96,32 @@ namespace SuggestedClasses
             Course course = section.ParentCourse;
             int recValue = 0;
 
-          
+            //foreach (requirement in student's requirements)
+            //if this course to that requirement's FulfillsRequirement method returns true, break loop and continue
+            //after loop if not found(maybe use found boolean variable), method returns 0
+            
+            if (Student.year < course.yearLvl) //1-Freshman, 2-Sophomore, 3-Junior, 4-Senior
+            {
+                return 0;
+            }
+
+            //Preferred that this course wouldn't send student over max credits (but not required because the student could always drop classes)
+            if (Student.totalCredits + course.credits <= 18)
+            {
+                recValue++;
+            }
+
+            //it is preferred that there are seats left
+            if (section.seats.seatPosition > 0)
+            {
+                recValue++;
+            }
+
+            //if there are 5 or less seats available, student should enroll soon
+            if (section.seats.seatPosition <= 5)
+            {
+                recValue++;
+            }
 
             return recValue;
         }
